3dced1076533845aaeddc2bd3d318179337bfcf9
Jessica Hunt <jessicahunt@Jessica-Hunts-MacBook-Pro.local>
2014-06-30 18:28:56 -0700

    newes javascript

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $('.carousel').carousel({
-  interval: 2000
+  interval: 1500
 })
+
+ $('.input-group.date').datepicker({
+        format: "yyyy/mm/dd",
+        startDate: "2012-01-01",
+        endDate: "2015-01-01",
+        todayBtn: "linked",
+        autoclose: true,
+        todayHighlight: true
+    });
